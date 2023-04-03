35e2f0cf16c3983f4f5636eb133afc2b3c551cf3
Rodrigo S. N. Mancini <rdgsnm@gmail.com>
2023-04-03 19:06:01 +0100

    final details before distribution

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
 from setuptools import setup, find_packages
-from pychrom import __version__, __author__
+
+
+__version__ = "0.0.6"
+__author__ = "Rodrigo Silva Nascimento Mancini"
+
 
 classifiers = [
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Science/Research',
-    'Operating System :: Microsoft :: Windows :: Windows 11',
-    'License :: OSI Approved :: MIT License',
-    'Natural Language :: English',
-    'Programming Language :: Python :: 3.10',
-    'Topic :: Scientific/Engineering :: Chemistry'
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Science/Research",
+    "Operating System :: Microsoft :: Windows :: Windows 11",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3.10",
+    "Topic :: Scientific/Engineering :: Chemistry",
 ]
 
 setup(
-    name='PyChrom',
+    name="PyChrom",
     version=__version__,
-    description='Module to provide tools to process and analyse chromatographic data from different sources such as UPLC, LCMS, GCMS, etc.',
-    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-    url='https://github.com/rdgsnm/PyChrom',
+    description="Module to provide tools to process and analyse chromatographic data from different sources such as UPLC, LCMS, GCMS, etc.",
+    long_description=open("README.txt").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    url="https://github.com/rdgsnm/PyChrom",
     author=__author__,
-    author_email='rdgsnm@gmail.com',
-    license='MIT',
+    author_email="rdgsnm@gmail.com",
+    license="MIT",
     classifiers=classifiers,
-    keywords='chromatography, chemometrics, data preprocessing',
+    keywords="chromatography, chemometrics, data preprocessing",
     packages=find_packages(),
-    install_requires=['numpy', 'scipy', 'sklearn']
+    install_requires=["numpy", "scipy", "sklearn"],
 )
